7cade4adcbccda97d93f7307cc557d9ee7245b7e
kaviyapriya30 <96899202+kaviyapriya30@users.noreply.github.com>
2022-01-10 11:47:12 +0530

    Update Demo.java

--- demo1/src/demo12/Demo.java
+++ demo1/src/demo12/Demo.java
@@ -5,5 +5,8 @@ public class Demo {
 	public static void main(String args[])
 	{
 		System.out.println("hello all");
+		
+		System.out.println("hello all");
+		System.out.println("hello all");
 	}
 }
